775b82b388f960239027597a6243f27874a1fd9a
Thomas Jaggi <thomas@responsive.ch>
2024-06-02 09:10:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Titel der Webseite
+
+## Untertitel
+
+Textblock
+
+## Weiterer Untertitel
+
+Textblock mit [Link zu PDF](./files/tmpFF7F.pdf).
---
+++ scripts/build.js
@@ -0,0 +1,44 @@
+import { cp, mkdir, readFile, rm, writeFile } from "node:fs/promises";
+import { dirname, resolve } from "node:path";
+import { marked } from "marked";
+
+const inputDir = resolve(import.meta.dirname, "..");
+const outputDir = resolve(import.meta.dirname, "../dist");
+
+async function createHtml() {
+  const inputPath = resolve(inputDir, "README.md");
+  const outputPath = resolve(outputDir, "index.html");
+
+  const input = await readFile(inputPath, "utf-8");
+
+  const output = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+${marked(input)}
+</body>
+</html>`;
+
+  await mkdir(dirname(outputPath), { recursive: true });
+  await writeFile(outputPath, output);
+}
+
+async function copyDirectory(dir) {
+  const inputPath = resolve(inputDir, dir);
+  const outputPath = resolve(outputDir, dir);
+
+  await cp(inputPath, outputPath, { recursive: true });
+}
+
+await rm(outputDir, {
+  recursive: true,
+});
+
+await createHtml();
+
+await copyDirectory("files");
+await copyDirectory("assets");
